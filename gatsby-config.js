87f5be752cb6346566d65159f01cbd8fad3b2693
Nik Davis <7122624+nik-davis@users.noreply.github.com>
2023-01-18 10:56:22 +0000

    Gatsby Tutorial Pt. 3: Images and Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `Rokanov Records Live`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Welcome to Home Page</p>
+      <StaticImage
+        alt="Rokanov Records Logo"
+        src="../images/logo-small.png"
+      />
     </Layout>
   )
 }
