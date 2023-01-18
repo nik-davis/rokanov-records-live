6d9a6790321a2a9114c68e3d8704bc77f2857118
Nik Davis <7122624+nik-davis@users.noreply.github.com>
2023-01-18 10:37:26 +0000

    Gatsby Tutorial Pt. 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>About us. Blah blah blah.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
